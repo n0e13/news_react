0fb4a9c4ea3393b68eb457d9af7bd5f590f92f02
n0e <noemy.garcia@gmail.com>
2022-04-27 21:14:28 +0200

    [ADD] Axios.get

--- src/components/Main/ListNews/Card/Card.jsx
+++ src/components/Main/ListNews/Card/Card.jsx
@@ -1,8 +1,24 @@
 import React, { Component } from "react";
 
 class Card extends Component {
+
   render() {
-    return <div>Card</div>;
+    const  news  = this.props.data;
+    const title = news.headline.main;
+    const subTitle = news.abstract;
+    const webUrl = news.web_url;
+    const source = news.source;
+    const imgSrc = `https://www.nytimes.com/${news.multimedia[0].url}`;
+    const date = news.pub_date;
+    const section = news.section_name;
+    const author = news.byline.person.firstname;
+
+    return (
+      <div>
+        Título: {title}, Sub:{subTitle} URL: {webUrl}, En: {source}, Fecha: {date}, Sección: {section}, Autor/a: {author}
+        <img src={imgSrc} alt='imagen' />
+      </div>
+    );
   }
 }
 
--- src/components/Main/ListNews/ListNews.jsx
+++ src/components/Main/ListNews/ListNews.jsx
@@ -1,9 +1,38 @@
 import React, { Component } from "react";
-//import Card from './Card';
+import { v4 as uuidv4 } from 'uuid';
+import axios from 'axios';
+import Card from './Card';
 
 class ListNews extends Component {
+  constructor(props) {
+    super(props)
+
+    this.state = {
+      news: []
+    }
+  }
+
+  componentDidMount = async () => {
+    const resp = await axios.get(`https://api.nytimes.com/svc/search/v2/articlesearch.json?&api-key=${process.env.REACT_APP_API_KEY}`);
+    const newsData = await resp.data.response.docs.slice(0, 5);;
+    this.setState({ news: newsData });
+  }
+
+  paintNews = () => {
+    return this.state.news.map((oneNew) => {
+      let id = uuidv4();
+      console.log('oneNew', oneNew);
+      return <Card data={oneNew} key={id} />
+    });
+  }
+
   render() {
-    return <div>ListNews</div>;
+    return (
+      <div>
+        <h1>Fakes News</h1>
+        {this.paintNews()}
+      </div>
+    );
   }
 }
 
